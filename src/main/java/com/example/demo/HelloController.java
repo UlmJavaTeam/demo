ccb24b7a8272f1f276a494c7bf991c2646bea4e4
OzanKilic6 <kilic.ozan@outlook.de>
2024-01-14 08:31:45 +0100

    initial

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,16 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-	@GetMapping("/HelloWorld")
-	public String index() {
-		return "Greetings from Spring Boot!";
+	@GetMapping("/SayHello")
+	public String hello(@RequestParam String name) {
+		return "Hello - " + name;
 	}
+
 
 }

519dc425b112280759e1747add4a291e138d284d
OzanKilic6 <kilic.ozan@outlook.de>
2024-01-15 22:17:15 +0100

    initial

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+		// hello
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
